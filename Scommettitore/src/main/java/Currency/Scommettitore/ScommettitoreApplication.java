e62a61e38736fe97033f01d0ac02a869329998ff
Edoardo Bilancia <s1087315@studenti.univpm.it>
2020-11-22 11:23:49 +0100

    Primo caricamento

---
+++ Scommettitore/src/main/java/Currency/Scommettitore/ScommettitoreApplication.java
@@ -0,0 +1,13 @@
+package Currency.Scommettitore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScommettitoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScommettitoreApplication.class, args);
+	}
+
+}
---
+++ Scommettitore/src/test/java/Currency/Scommettitore/ScommettitoreApplicationTests.java
@@ -0,0 +1,13 @@
+package Currency.Scommettitore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScommettitoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
