cce2afbfcd6f6473a9e91b5cd5b5c15138623da6
Emanuele Biccheri <s1087650@studenti.univpm.it>
2020-11-22 11:59:06 +0100

    bello1

--- Scommettitore/src/main/java/Currency/Scommettitore/ScommettitoreApplication.java
+++ Scommettitore/src/main/java/Currency/Scommettitore/ScommettitoreApplication.java
@@ -8,7 +8,7 @@ public class ScommettitoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScommettitoreApplication.class, args);
-		System.out.println("ciao bello belloooo");
+		System.out.println("ciao bello belloooo1");
 	}
 
 }
