4c1aacbd19338b62762f3c851be8b7460b083f99
Edoardo Bilancia <s1087315@studenti.univpm.it>
2020-11-26 11:48:42 +0100

    commit2boh

--- Scommettitore/src/main/java/currency/scommettitoreApp/service/PrincipalService.java
+++ Scommettitore/src/main/java/currency/scommettitoreApp/service/PrincipalService.java
@@ -1,6 +1,8 @@
 package currency.scommettitoreApp.service;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Vector;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -12,18 +14,25 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 
-import currency.scommettitoreApp.model.Currencylayer;
-import currency.scommettitoreApp.model.Data;
+import currency.scommettitoreApp.model.*;
 
 public class PrincipalService {
 	
-	HashMap
 	
-	public static HashMap<String,ModelloValuta> CiclaDate(String from, String to) {
+	static Vector<ApiParsing> p = new Vector<ApiParsing>();
+	static HashMap<String,Vector<ApiParsing>> hs = new HashMap<String,Vector<ApiParsing>>();	
+	public static HashMap<String,Vector<ApiParsing>> CiclaDate(String from, String to) {
 		String giorni[] = Data.datatostring(from,to);
 		for(String x : giorni) {
-			Currencylayer.getJson(urlService.geturl(x));
+			try {
+				p.add(Currencylayer.getJson(urlService.geturl(x)));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			hs.put(x,p);
+		}
+		return hs;
 			
 		}
 	}
-}
